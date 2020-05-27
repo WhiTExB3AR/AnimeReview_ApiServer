15e1dd98f50ac288b1e587833e2ac6437df20189
WhiTE._.BeAR <62128227+WhiTEBeAR-99@users.noreply.github.com>
2020-05-27 09:05:10 +0700

    Update Introduce

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const Manga = require('../models/manga')
 
 router.get('/',(req, res) =>{
     res.render('index') //file layout.ejs trong layouts
