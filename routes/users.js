d67ab036043d02ec2b70279998b2cf47d5833294
WhiTE._.BeAR <62128227+WhiTEBeAR-99@users.noreply.github.com>
2020-05-31 13:49:17 +0700

    View Edit and Delete user

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,9 @@ const router = express.Router()
 const User = require('../models/user')
 
 //Route dùng để load toàn bộ người dùng trong database
+// router.get('/',(req, res) =>{ //Dùng async để bắt lỗi và cho code nhìn đẹp hơn
+//     res.render('Users/index')
+// })
 router.get('/', async (req, res) => {
   let searchOptions = {}
   if (req.query.name != null && req.query.name !== '') {
@@ -18,9 +21,6 @@ router.get('/', async (req, res) => {
     res.redirect('/')
   }
 })
-// router.get('/',(req, res) =>{ //Dùng async để bắt lỗi và cho code nhìn đẹp hơn
-//     res.render('Users/index')
-// })
 
 //Route dùng để load 1 người dùng cụ thể trong databse
 router.get('/new', (req, res) => {
@@ -30,24 +30,6 @@ router.get('/new', (req, res) => {
 })
 
 //Route dùng để tạo mới người dùng trong database
-router.post('/', async (req, res) => {
-  const user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.pwd
-  })
-  try {
-    const newUser = await user.save();
-    // res.redirect(`users/${newUser.id}`)
-    res.redirect(`users`);
-  } catch (error) {
-    console.log(error)
-    res.render('users/new', {
-      user: user,
-      errorMessage: 'Error creating User',
-    })
-  }
-})
 //router.post('/', (req, res) =>{  //Bắt lỗi theo code bên dưới sẽ hơi cồng kềnh, dùng JavaScript Async Await
 //    const user = new User ({
 //            name: req.body.name,
@@ -66,5 +48,85 @@ router.post('/', async (req, res) => {
 //     }
 // })
 //})
+router.post('/', async (req, res) => {
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.pwd
+  })
+  try {
+    const newUser = await user.save()
+    res.redirect(`users/${newUser.id}`)
+  } catch (error) {
+    console.log(error)
+    res.render('users/new', {
+      user: user,
+      errorMessage: 'Error creating User',
+    })
+  }
+})
+
+router.get('/:id', async (req, res) => { //lấy id user từ trang gốc
+  // res.send('Show User '+ req.params.id)
+  try {
+    const user = await User.findById(req.params.id)
+    res.render('users/show', { user: user })
+  } catch (error) {
+    console.log(error)
+    res.redirect('/')
+  }
+})
+
+router.get('/:id/edit', async (req, res) => {
+  // res.send('Edit User ' + req.params.id)
+  try {
+    const user = await User.findById(req.params.id)
+    res.render('users/edit', { user: user })
+  } catch  {
+    res.redirect('/users')
+  }
+
+})
+
+router.put('/:id', async (req, res) => { //không thêm /edit vào vì giao thức PUT đã mặc định là update/edit rồi
+  // res.send('Update User ' + req.params.id)
+  let user
+  try {
+    user = await User.findById(req.params.id)
+    //thêm vào các trường để update 
+    user.name = req.body.name
+    user.email = req.body.email
+    user.password = req.body.pwd
+    await user.save()
+    res.redirect(`/users/${user.id}`)
+  } catch (error) {
+    console.log(error)
+    if (user == null) {
+      res.redirect('/')
+    } else {
+      res.render('users/edit', {
+        user: user,
+        errorMessage: 'Error updating User',
+      })
+    }
+  }
+})
+
+router.delete('/:id', async (req, res) => { //không thêm /delete vào vì giao thức DELETE đã mặc định là delete rồi
+  // res.send('Delete User ' + req.params.id)
+  let user
+  try {
+    user = await User.findById(req.params.id)
+    await user.remove()
+    res.redirect('/users')
+  } catch (error) {
+    console.log(error)
+    if (user == null) {
+      res.redirect('/')
+    } else {
+      res.redirect(`/users/${user.id}`)
+    }
+  }
+})
 
 module.exports = router
